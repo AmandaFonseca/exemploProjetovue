ca453d694fb25cc8f230b7cbab816b080d5111ef
AmandaFonseca <amandafonseca207@gmail.com>
2020-09-29 23:37:42 -0300

    tratamento pagina 404

--- app-vue/src/pages/Error404.vue
+++ app-vue/src/pages/Error404.vue
@@ -1,13 +1,9 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+  <div class="fullscreen text-white text-center q-pa-md flex flex-center">
     <div>
-      <div style="font-size: 30vh">
-        404
-      </div>
+      <div style="font-size: 30vh">404</div>
 
-      <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
-      </div>
+      <div class="text-h2" style="opacity: 0.4">Opa. Nada aqui...</div>
 
       <q-btn
         class="q-mt-xl"
@@ -15,17 +11,26 @@
         text-color="blue"
         unelevated
         to="/"
-        label="Go Home"
+        label="Voltar"
         no-caps
       />
     </div>
   </div>
 </template>
 
+<style scoped>
+.fullscreen {
+  background-color: #ff9999;
+}
+</style>
+
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import { defineComponent } from "@vue/composition-api";
 
 export default defineComponent({
-  name: 'Error404',
+  name: "Error404",
+  created() {
+    this.$router.push({ path: "/" });
+  },
 });
 </script>
